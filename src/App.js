e81b1cf8f7a7cc9a0f8def48fbc9995ddecc6703
John Doe <lucas.lopez.borchardt@gmail.com>
2022-08-08 15:13:12 +0200

    files, App.js, IsAnon, IsPrivate, .env

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticleDetailsPage from "./pages/ArticleDetailsPage";
+import AddArticle from "./components/AddArticle";
+import EditArticle from "./components/EditArticle";
+import CategoryListPage from "./components/CategoryListPage";
+import AddCategory from "./components/AddCategory";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import IsPrivate from './components/IsPrivate';
+import IsAnon from "./components/IsAnon";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={ <HomePage />} />
+        <Route path="/articles" element={ <ArticleListPage />} />
+        <Route path="/articles/:articleId" elemant={ <ArticleDetailsPage />} />
+        <Route path="/articles/:articleId/add" element={ <IsPrivate>
+          <AddArticle />
+        </IsPrivate>} />
+        <Route path="/articles/:articleId/edit" element={ <IsPrivate>
+          <EditArticle />
+        </IsPrivate>} />
+        <Route path="/categories" element={ <CategoryListPage />} />
+        <Route path="/categories/add" element={ <IsPrivate>
+          <AddCategory />
+        </IsPrivate>} /> 
+        <Route path="/signup" element={ <IsAnon>
+            <SignupPage />
+          </IsAnon> } />
+        <Route path="/login" element={ <IsAnon>
+            <LoginPage />
+          </IsAnon> } /> 
+      </Routes>
+
     </div>
   );
 }
